cac6cdb0e3ba9521d35d305ebb310e6df71244a0
fiftysev <nesgorai@bk.ru>
2021-11-30 02:54:29 +1000

    Add middlewares

--- index.ts
+++ index.ts
@@ -1,13 +1,18 @@
 require("dotenv").config();
 import express from "express";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import mongoose from "mongoose";
+
 import { userRouter } from "./src/routers/user.router";
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 app.use("/user", userRouter);
 
 app.get("/", (req: express.Request, res: express.Response) => {
