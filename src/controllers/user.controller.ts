5b6591c7b27c76cd1ac0a236bbf50e8f82e37deb
fiftysev <nesgorai@bk.ru>
2021-11-30 01:51:58 +1000

    Create Registration method in user.controller.ts

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,11 +1,35 @@
 import { Request, Response, NextFunction } from "express";
-import { IUser } from "../models/interfaces/user.interface";
 import { User } from "../models/user.model";
+import userService from "../services/user.service";
 
 export class UserController {
   constructor() {}
   async registration(req: Request, res: Response, next: NextFunction) {
-    const user = req.body as IUser;
-    console.log(user);
+    try {
+      const { firstName, lastName, username, password } = req.body;
+
+      const candidate = await User.findOne({ username });
+      if (candidate) {
+        return res
+          .status(400)
+          .json({ error: `User ${username} is already exists` });
+      }
+
+      const userData = await userService.registration(
+        firstName,
+        lastName,
+        username,
+        password
+      );
+
+      res.cookie("refreshToken", userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      });
+
+      return res.status(200).json(userData);
+    } catch (e) {
+      next(e);
+    }
   }
 }
