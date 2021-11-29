163f46fc59cbc947e7829d4169d8575989197059
fiftysev <nesgorai@bk.ru>
2021-11-30 01:49:09 +1000

    Create base userRouter

--- index.ts
+++ index.ts
@@ -2,11 +2,13 @@ require("dotenv").config();
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import { userRouter } from "./src/routers/user.router";
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use("/user", userRouter);
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Main route");
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user.controller";
 
-const userRouter = Router();
+export const userRouter = Router();
 const controller = new UserController();
 
 userRouter.post("/register", controller.registration);
