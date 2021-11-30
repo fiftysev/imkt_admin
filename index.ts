1daf524025878746b7c7a9d68ebfc799f798c8d3
fiftysev <nesgorai@bk.ru>
2021-11-30 20:54:32 +1000

    Modify cors on server

--- index.ts
+++ index.ts
@@ -13,7 +13,12 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+    origin: "*",
+  })
+);
 app.use("/auth", userRouter);
 app.use(errorMiddleware);
 
