3e48c08f72f8510672bd7f3405b55cbbbad3392e
fiftysev <nesgorai@bk.ru>
2021-12-01 20:42:32 +1000

    Add state hooks for forms

--- client/src/components/SignInForm.tsx
+++ client/src/components/SignInForm.tsx
@@ -2,9 +2,12 @@ import { Button } from "@chakra-ui/button";
 import { FormControl, FormLabel } from "@chakra-ui/form-control";
 import { Input } from "@chakra-ui/input";
 import { Box, Heading, VStack } from "@chakra-ui/layout";
-import { FC } from "react";
+import { FC, useState } from "react";
 
 export const SignInForm: FC = () => {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
   return (
     <Box p={8} minW={"500px"} borderWidth={1} borderRadius={8} boxShadow={"lg"}>
       <Heading size={"lg"} textAlign={"center"} mb={4}>
@@ -14,11 +17,21 @@ export const SignInForm: FC = () => {
         <VStack spacing={4}>
           <FormControl isRequired>
             <FormLabel>Имя пользователя</FormLabel>
-            <Input type="text" placeholder="IvanovIv23" />
+            <Input
+              type="text"
+              value={username}
+              placeholder="IvanovIv23"
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </FormControl>
           <FormControl isRequired>
             <FormLabel>Пароль</FormLabel>
-            <Input type="password" placeholder="*******" />
+            <Input
+              type="password"
+              value={password}
+              placeholder="*******"
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </FormControl>
           <Button colorScheme={"green"} w={"full"} type="submit">
             Войти
--- client/src/components/SignUpForm.tsx
+++ client/src/components/SignUpForm.tsx
@@ -1,9 +1,14 @@
 import { Button } from "@chakra-ui/button";
 import { Box, HStack, VStack } from "@chakra-ui/layout";
 import { Heading, FormControl, FormLabel, Input } from "@chakra-ui/react";
-import { FC } from "react";
+import { FC, useState } from "react";
 
 export const SignUpForm: FC = () => {
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
   return (
     <Box p={8} minW={"500px"} borderWidth={1} borderRadius={8} boxShadow={"lg"}>
       <Heading size={"lg"} textAlign={"center"} mb={4}>
@@ -14,20 +19,40 @@ export const SignUpForm: FC = () => {
           <HStack spacing={2}>
             <FormControl isRequired>
               <FormLabel>Имя</FormLabel>
-              <Input type="text" placeholder="Иван" />
+              <Input
+                type="text"
+                value={firstName}
+                placeholder="Иван"
+                onChange={(e) => setFirstName(e.target.value)}
+              />
             </FormControl>
             <FormControl isRequired>
               <FormLabel>Фамилия</FormLabel>
-              <Input type="text" placeholder="Иванов" />
+              <Input
+                type="text"
+                value={lastName}
+                placeholder="Иванов"
+                onChange={(e) => setLastName(e.target.value)}
+              />
             </FormControl>
           </HStack>
           <FormControl isRequired>
             <FormLabel>Имя пользователя</FormLabel>
-            <Input type="text" placeholder="IvanovIv23" />
+            <Input
+              type="text"
+              value={username}
+              placeholder="IvanovIv23"
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </FormControl>
           <FormControl isRequired>
             <FormLabel>Пароль</FormLabel>
-            <Input type="password" placeholder="*******" />
+            <Input
+              type="password"
+              value={password}
+              placeholder="*******"
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </FormControl>
           <Button colorScheme={"green"} w={"full"} type="submit">
             Войти
