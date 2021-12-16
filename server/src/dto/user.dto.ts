c3d5d92f249d080a4cb33855746ba9cb1fda410e
fiftysev <nesgorai@bk.ru>
2021-12-16 22:48:27 +1000

    Minor fix user DTO

--- server/src/dto/user.dto.ts
+++ server/src/dto/user.dto.ts
@@ -3,13 +3,9 @@ import { Types } from "mongoose";
 export class UserDTO {
   id: Types.ObjectId;
   username: string;
-  firstName: string;
-  lastName: string;
 
   constructor(model: any) {
     this.id = model._id;
     this.username = model.username;
-    this.firstName = model.firstName;
-    this.lastName = model.lastName;
   }
 }
